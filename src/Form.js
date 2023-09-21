25e20104d47f02d35b8b7a54a0de651b357751d5
Jaures Tankeu Nzeyem <jaures.tankeunzeyem@cgi.com>
2023-09-21 13:37:34 -0400

    Implement and use bootstrap

--- src/Form.js
+++ src/Form.js
@@ -1,68 +1,26 @@
 import { Component } from "react";
-import Car from "./Car";
+import { styled } from "styled-components";
+
+const Title = styled.h1`
+  color: red;
+  font-size: 80px;
+`;
+
+const Button = styled.button`
+  background: #BF4F74;
+  border-radius: 3px;
+  border: 2px solid #BF4F74;
+  color: white;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+`;
 
 export class Form extends Component {
-  state = {
-    username: "",
-    color: "",
-    colors: ["", "red", "blue", "green", "black"],
-    comment: ''
-  };
-
-  handlePseudo = (e) => {
-    this.setState({
-      username: e.target.value,
-    });
-  };
-
-  handleColor = (e) => {
-    this.setState({
-      color: e.target.value,
-    });
-  };
-  
-  handleComment = (e) => {
-    this.setState({
-      comment: e.target.value,
-    });
-  };
-
-  handleSubmitForm=(e)=>{
-    e.preventDefault();
-    console.log(e);
-  }
-
   render() {
     return (
       <div>
-        <Car color="red" height="200" />
-        <h1>Commentaire</h1>
-        <form onSubmit={this.handleSubmitForm}>
-          <div>
-            <label>Pseudo</label>
-            <input
-              type="text"
-              value={this.state.username}
-              onChange={this.handlePseudo}
-            />
-          </div>
-
-          <div>
-            <label>Couleur</label>
-            <select value={this.state.color} onChange={this.handleColor}>
-              {this.state.colors.map((c, k) => {
-                return <option key={k} value={c}>{c}</option>;
-              })}
-            </select>
-          </div>
-
-          <div>
-            <label>Commentaire</label>
-            <textarea value={this.state.comment} onChange={this.handleComment}></textarea>
-          </div>
-
-          <button>Valider</button>
-        </form>
+        <Title>Commentaire</Title>
+        <Button>Valider</Button>
       </div>
     );
   }
