c664fa53367459f6ec0f20a925f9b8dd1deb474f
Jaures Tankeu Nzeyem <jaures.tankeunzeyem@cgi.com>
2023-09-25 09:51:29 -0400

    Les composant d'ordre supérieur

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import "./App.css";
 import { Component } from "react";
-import { Form } from "./Form";
+import Vegeta from "./components/Vegeta";
+import Goku from "./components/Goku";
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      vegeta: 100,
+      goku: 100
+    }
+  }
+
+  reduceLife = (param, power) => {
+    if (param === 'Goku') {
+      this.setState({
+        vegeta: this.state.vegeta - power
+      })
+    } else {
+      this.setState({
+        goku: this.state.goku - power
+      })
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <Form />
+      <div className="container text-center">
+        <h1>Goku vs Vegeta</h1>
+
+        <hr />
+
+        <div className="row">
+          <Vegeta name="Vegeta" life={this.state.vegeta} reduceHandler={this.reduceLife} />
+          <Goku name='Goku' life={this.state.goku} reduceHandler={this.reduceLife} />
+        </div>
       </div>
     );
   }
